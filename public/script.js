9250bdf85a76013b08d5b947edc9f4be44cd86ad
thakur-ritik <ritikthakur1177@gmail.com>
2026-04-21 00:47:24 +0530

    Refactor script.js to include HTML structure

--- public/script.js
+++ public/script.js
@@ -1,17 +1,29 @@
-fetch('/api/posts')
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Post</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="post-page">
+  <img id="image" />
+  <h1 id="title"></h1>
+  <p id="content"></p>
+</div>
+
+<script>
+const params = new URLSearchParams(window.location.search);
+const id = params.get('id');
+
+fetch(`/api/posts/${id}`)
   .then(res => res.json())
   .then(data => {
-    const container = document.getElementById('posts');
-
-    data.forEach(post => {
-      const div = document.createElement('div');
-      div.className = 'card';
-      div.innerHTML = `<h3>${post.title}</h3><p>${post.content.substring(0, 60)}...</p>`;
-      
-      div.onclick = () => {
-        window.location.href = `post.html?id=${post.id}`;
-      };
-
-      container.appendChild(div);
-    });
+    document.getElementById("title").innerText = data.title;
+    document.getElementById("content").innerText = data.content;
+    document.getElementById("image").src = data.image;
   });
+</script>
+
+</body>
+</html>
